eb1cc8e516f6d1e53c3be478bc77af4aba2572ee
Alessandro <alexpiazza98@gmail.com>
2026-06-04 14:27:23 +0200

    cominciata homepage

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -3,9 +3,11 @@ import "../css/homepage.css";
 export default function Homepage() {
   return (
     <>
-      <h1 className=" altezza_minima">
-        Prova Tailwind CSS
-      </h1>
+      <div>
+        <div className="sfondo_homepage flex items-center justify-center"> 
+          <h1 className="text-5xl font-bold testo_bianco ">Benvenuti su Casse Automatiche</h1>
+        </div>
+      </div>
     </>
   );
 }
