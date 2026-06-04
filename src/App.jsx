004a239c2d98751095e0f9fd252ee4bc570d8f4d
Alessandro <alexpiazza98@gmail.com>
2026-06-04 13:01:49 +0200

    inizio progetto, implementato correttamente tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-500 bg-yellow-200 p-10 underline">
+        Se vedi questo rosso su sfondo giallo, Tailwind funziona!
+      </h1>
+    </>
+  );
+}
