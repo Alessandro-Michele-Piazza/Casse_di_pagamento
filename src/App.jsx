4532122acee2c8d0d9ba9e700190b0f0e3ffc1b8
Alessandro <alexpiazza98@gmail.com>
2026-06-04 13:08:55 +0200

    inizio palette colori ecc

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-red-500 bg-yellow-200 p-10 underline">
-        Se vedi questo rosso su sfondo giallo, Tailwind funziona!
+      <h1 className="">
+        Prova Tailwind CSS
       </h1>
     </>
   );
