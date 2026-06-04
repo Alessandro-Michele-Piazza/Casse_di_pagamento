ebbaae20b1b6365d70c611b28cd013f3b36ca199
Alessandro <alexpiazza98@gmail.com>
2026-06-04 14:42:24 +0200

    impostatoscafholding

--- src/views/homepage/Homepage.jsx
+++ src/views/homepage/Homepage.jsx
@@ -1,13 +1,10 @@
 import "../../css/homepage.css";
+import Header from "./Header";
 
 export default function Homepage() {
   return (
     <>
-      <div>
-        <div className="sfondo_homepage flex items-center justify-center"> 
-          <h1 className="text-5xl font-bold testo_bianco ">Benvenuti su Casse Automatiche</h1>
-        </div>
-      </div>
+      <Header />
     </>
   );
 }
