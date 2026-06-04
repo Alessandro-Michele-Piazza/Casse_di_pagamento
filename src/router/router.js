05fdf0b42aa3f5c35f1667884e96c163b4b6106f
Alessandro <alexpiazza98@gmail.com>
2026-06-04 13:41:12 +0200

    fixato un erroe di battitura sul file homepage

---
+++ src/router/router.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+
+        path: "/",
+        Component: () => <h1>Home</h1>
+
+
+
+  } 
+
+
+]);
+
+export default router;
